791edf0c78b12c628e2c05a656fab7640652dd30
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-19 14:24:19 +0300

    Refactor UI and logic for HomePage and DialogBox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/utils/dialog_box.dart';
 import 'package:to_do_app/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,35 +22,26 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void createTask() {
-    showDialog(
-      context: context,
-      builder: ((context) {
-        return DialogBox();
-      }),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('To Do App'),
-        backgroundColor: Colors.amberAccent,
-      ),
-      body: ListView.builder(
-        itemCount: toDoitems.length,
-        itemBuilder: ((context, index) {
-          return ToDoTile(
-            taskName: toDoitems[index][0],
-            isDone: toDoitems[index][1],
-            onChanged: (value) => checkBoxState(value, index),
-          );
-        }),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: createTask,
-        child: const Icon(Icons.add),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('To Do App'),
+          backgroundColor: Colors.amberAccent,
+        ),
+        body: ListView.builder(
+          itemCount: toDoitems.length,
+          itemBuilder: ((context, index) {
+            return ToDoTile(
+              taskName: toDoitems[index][0],
+              isDone: toDoitems[index][1],
+              onChanged: (value) => checkBoxState(value, index),
+            );
+          }),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
       ),
     );
   }
